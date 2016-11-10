d069160f48bb7e3fc861aa9d82e5335e5d8b2a50
sramayanam <sreekanth_bhr@yahoo.com>
2016-11-10 08:09:26 -0600

    Adding Create device functionality

---
+++ ProcessDeviceToCloudMessages/ProcessDeviceToCloudMessages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ProcessDeviceToCloudMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iotHubConnectionString = "HostName=srramdemo.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=np+WT8KRP41II8lxdvoSXkTEXTlW1v3glwIQR9wo2CU=";
+            string iotHubD2cEndpoint = "messages/events";
+            StoreEventProcessor.StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storagefordemodfsrram;AccountKey=RUTukQbuykqb1LS1+3Az4rubAbuS/gY1N8b3nNvKg+HPdSW0TZtbk6PvCOyvQqNj8SOJvAYv7f/T+5icX+5/nQ==;BlobEndpoint=https://storagefordemodfsrram.blob.core.windows.net/;TableEndpoint=https://storagefordemodfsrram.table.core.windows.net/;QueueEndpoint=https://storagefordemodfsrram.queue.core.windows.net/;FileEndpoint=https://storagefordemodfsrram.file.core.windows.net/";
+            StoreEventProcessor.ServiceBusConnectionString = "HostName=srramdemo.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=Wj7vlYAiMTQIjdDe+niS3IRmn6ZoTUU9rYeEisZjmHU=";
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, iotHubD2cEndpoint, EventHubConsumerGroup.DefaultGroupName, iotHubConnectionString, StoreEventProcessor.StorageConnectionString, "messages-events");
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<StoreEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ ReadDeviceToCloudMessages/ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReadDeviceToCloudMessages
+{
+    class Program
+    {
+
+        static string connectionString = "HostName=srramiothub.azure-devices.net;DeviceId=rb2;SharedAccessKey=owGSUyvpUR2Cg19itAskk44/hkHbka6Dk4rgf8A0lnY=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+        private async static Task ReceiveMessagesFromDeviceAsync(string partition)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine(string.Format("Message received. Partition: {0} Data: '{1}'", partition, data));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            foreach (string partition in d2cPartitions)
+            {
+                ReceiveMessagesFromDeviceAsync(partition);
+            }
+           // Console.ReadLine();
+
+        }
+    }
+}
